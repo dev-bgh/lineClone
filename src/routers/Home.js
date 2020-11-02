0dc9d0e946866916b9eee1efa910916024521340
tom <tom@publishsoft.io>
2020-11-02 15:03:17 +0900

    깃 테스트

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const Home = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>LoginScreenasd</Text>
+      <Text>LoginScreenaesd</Text>
     </View>
   );
 }
