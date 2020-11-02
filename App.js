5859f5046607c6d54c88d33bd8f810ad8b4c35c3
tom <tom@publishsoft.io>
2020-11-02 14:56:24 +0900

    라인 클론 시작

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import Home from './src/routers/Home';
+
+function App() {
+  return (
+     <Home />
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
